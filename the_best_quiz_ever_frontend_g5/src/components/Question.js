6a5d5f5985a6c53fd964db046b75553f6f6a69d9
Albert Hughes <albieh@me.com>
2023-12-11 18:08:14 +0000

    fixed question component to display answers

--- the_best_quiz_ever_frontend_g5/src/components/Question.js
+++ the_best_quiz_ever_frontend_g5/src/components/Question.js
@@ -1,41 +1,21 @@
-import { act } from "react-dom/test-utils"
+
 import Answer from "./Answer"
 
 const Question = ({activeQuestion}) => {
 
-    // const checkActiveQuestion = () => {
-    //     const answerComponents = activeQuestion.nextQuestion.answers.map ((answer) => {
-    //         return <Answer answer={answer} key={answer.id}/>
-    //     })
-    //     if(activeQuestion){
-    //         return (
-    //         <>
-    //         <h2>{activeQuestion.nextQuestion.questionText}</h2>
-    //         <ul>{activeQuestion ? checkActiveQuestion() : <p>Loading... </p> }</ul>
-    //         </>
-    //         ) 
-    //     } else {
-    //         return <p>Loading...</p>
-    //     }
-    // } 
-
-    
-    
-    const checkAnswerComponents = () => {
-        if (activeQuestion) {
-            const answerComponents = activeQuestion.nextQuestion.answers.map ((answer) => {
-                return <Answer answer={answer} key={answer.id}/>
-            })
-        } else {
-            <p>Loading...</p>
-        }
+    if (!activeQuestion) {
+        return <p>Loading...</p>;
     }
 
-    
+    const answerComponents = activeQuestion.nextQuestion.answers.map ((answer) => {
+                console.log(answer)
+                return (<Answer answer={answer} key={answer.id}/>)
+            })
+        
     return ( 
         <>
-        {activeQuestion ? <h2>{activeQuestion.nextQuestion.questionText}</h2> :<p>Loading...</p> }
-        {activeQuestion ? checkAnswerComponents(): <p>Test</p>}
+        <h2>{activeQuestion.nextQuestion.questionText}</h2>
+        {answerComponents}
         </>
     );
 
