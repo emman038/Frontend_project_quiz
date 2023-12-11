5b2d2bbd4675820f0337996c0e9f32656825245e
Albert Hughes <albieh@me.com>
2023-12-11 12:06:17 +0000

    fixed fetch error.

--- the_best_quiz_ever/src/main/java/com/example/the_best_quiz_ever/configurations/SpringGlobalConfig.java
+++ the_best_quiz_ever/src/main/java/com/example/the_best_quiz_ever/configurations/SpringGlobalConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class SpringGlobalConfig {
+public class SpringGlobalConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry){
 
--- the_best_quiz_ever_frontend_g5/src/containers/QuizContainer.js
+++ the_best_quiz_ever_frontend_g5/src/containers/QuizContainer.js
@@ -1,8 +1,33 @@
 import Question from "../components/Question";
 import QuizList from "../components/QuizList";
 import Result from "../components/Result";
+import { useEffect, useState } from "react";
 
 const QuizContainer = () => {
+
+
+const [quizList, setQuizList] = useState([]);
+const [currentQuiz, setCurrentQuiz] = useState({});
+
+
+const fetchQuizList = async () => {
+    const response = await fetch("http://localhost:8080/quizzes");
+    const data = await response.json();
+    setQuizList(data);
+}
+
+useEffect(() => {
+    fetchQuizList();
+}, [])
+
+
+
+
+
+
+
+
+
     return ( 
         <>
             <h1>Hello from Container</h1>
