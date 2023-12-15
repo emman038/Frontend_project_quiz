77180bf3c23c3b9d156ff225bf7fa371cc16301a
Rayster Ramos <rjrfrst@gmail.com>
2023-12-15 10:04:16 +0000

    added comments for presentation

--- the_best_quiz_ever_frontend_g5/src/components/QuizList.js
+++ the_best_quiz_ever_frontend_g5/src/components/QuizList.js
@@ -2,6 +2,7 @@ import QuizCard from "./QuizCard";
 
 const QuizList = ({quizList, handleStartQuiz, findCurrentQuestion}) => {
 
+    // Map through the quizzes and create a quiz card for each quiz
     const quizComponents = quizList.map((quiz) => {
         return <QuizCard key={quiz.id} quiz={quiz} handleStartQuiz={handleStartQuiz} findCurrentQuestion={findCurrentQuestion} />
     })
